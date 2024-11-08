34bcbe7c5ade5a8ba798fe76c22c90caaea7ab79
jimmyboy722 <jeckman722@gmail.com>
2024-11-07 23:31:41 -0500

    added necessary dependencies in client package.json, added and commented on search and save react components,added imports and two components to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,50 @@ import Header from "./components/Header.jsx";
 import SignUpForm from "./components/SignupForm.jsx";
 import AddDrink from "./components/AddDrink.jsx";
 import LoginForm from "./components/LoginForm.jsx";
+import SearchDrink from "./components/SearchDrinks.jsx";
+import SavedDrink from "./components/SavedDrinks.jsx";
+
+import {Outlet} from "react-router-dom";
+import '../src/app.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const tkn = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: tkn ? `Bearer ${tkn}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
-    <div>
+    <ApolloProvider client={client}>
       <Header/>
       <SignUpForm/>
       <LoginForm/>
       <AddDrink/>
-    </div>
+      <SearchDrink/>
+      <SavedDrink/>
+      <Outlet/>
+    </ApolloProvider>
   );
 }
 
