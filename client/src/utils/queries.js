b28e889c0ec9ff239f69bfc000671c7ef96de84a
emilyrose18 <emilyramirez218@gmail.com>
2024-11-11 22:12:31 -0500

    added a add drink page

---
+++ client/src/utils/queries.js
@@ -0,0 +1,70 @@
+import { gql} from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      drinks {
+        _id
+        drinkName
+        drinkAuthor
+        createdAt
+        drinkDescription
+      }
+    }
+  }
+`;
+export const QUERY_DRINKS = gql`
+query Drinks {
+    drinks {
+      drinkName
+      drinkDescription
+      drinkAuthor
+      createdAt
+      _id
+      ingredients {
+        name
+        quantity
+        unit
+      }
+      photo
+    }
+  }
+  `;
+
+
+export const QUERY_USER = gql`
+query User($username: String!) {
+  user(username: $username) {
+    _id
+    drinks {
+      _id
+      createdAt
+      drinkAuthor
+      drinkDescription
+      drinkName
+      ingredients {
+        name
+        quantity
+        unit
+      }
+    }
+    email
+    favorites {
+      _id
+      createdAt
+      drinkAuthor
+      drinkDescription
+      drinkName
+      ingredients {
+        name
+        quantity
+        unit
+      }
+    }
+    username
+  }
+}
+`;
