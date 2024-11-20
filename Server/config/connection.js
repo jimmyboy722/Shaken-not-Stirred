540d3767b14fefd7fbc45f37e08d547795a92210
jimmyboy722 <jeckman722@gmail.com>
2024-11-19 22:49:27 -0500

    added mongodb package and updated mongo env variable

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,7 +1,7 @@
 // IMPORTING MONGOOSE
 const mongoose = require("mongoose");
 // CONNECTING TO MONGODB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/");
+mongoose.connect(process.env.MONGODB_URI);
 
 // EXPORTING THE CONNECTION
 module.exports = mongoose.connection;
